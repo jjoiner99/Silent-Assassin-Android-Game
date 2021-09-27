fbe27b580b451f983fd25a42a037d3effeb22fcd
Jacob Diaz <hello@jacobdiaz.dev>
2021-09-27 16:59:52 -0500

    chore: Created basic functioning map to point to UIC

--- app/src/main/java/com/example/cs440project/MainActivity.java
+++ app/src/main/java/com/example/cs440project/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.cs440project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide(); // hide the title bar
         setContentView(R.layout.activity_main);
+    }
 
+    public void handleStartBtn(View view) {
+        Intent startMapScreen = new Intent(MainActivity.this, MapsActivity.class);
+        MainActivity.this.startActivity(startMapScreen);
     }
 }
