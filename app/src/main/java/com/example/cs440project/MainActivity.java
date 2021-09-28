4a121d862f7468b3877c31c382c3b30639285265
VivaceJamiel <jimpoy2@gmail.com>
2021-09-28 12:20:03 -0500

    fixed button after gaining permissions

--- app/src/main/java/com/example/cs440project/MainActivity.java
+++ app/src/main/java/com/example/cs440project/MainActivity.java
@@ -32,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST);
+        } else {
+            Intent startMapScreen = new Intent(MainActivity.this, MapsActivity.class);
+            MainActivity.this.startActivity(startMapScreen);
         }
     }
 
