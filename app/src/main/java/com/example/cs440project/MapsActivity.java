75d2da63098aee4ea3f5f80bffb694214083e988
VivaceJamiel <jimpoy2@gmail.com>
2021-10-07 22:13:18 -0500

    added pushing, but not tested

--- app/src/main/java/com/example/cs440project/MapsActivity.java
+++ app/src/main/java/com/example/cs440project/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.cs440project;
 import android.annotation.SuppressLint;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -14,11 +15,16 @@ import com.example.cs440project.interestPoints.InterestPoints;
 import com.example.cs440project.locationCheck.locationCheck;
 import com.example.cs440project.mapPreference.MapPreference;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MapsActivity extends FragmentActivity
         implements OnMapReadyCallback,
@@ -26,6 +32,10 @@ public class MapsActivity extends FragmentActivity
         GoogleMap.OnMyLocationClickListener {
 
     private FusedLocationProviderClient FSL;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+    private Location currentLocation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +48,8 @@ public class MapsActivity extends FragmentActivity
                 .findFragmentById(R.id.map);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
+        createLocationRequest();
+        createLocationCallback();
         FSL = LocationServices.getFusedLocationProviderClient(this);
     }
 
@@ -54,6 +66,7 @@ public class MapsActivity extends FragmentActivity
         googleMap.setMyLocationEnabled(true);
         googleMap.setOnMyLocationButtonClickListener(this);
         googleMap.setOnMyLocationClickListener(this);
+        startLocationUpdates();
     }
 
     @Override
@@ -71,13 +84,41 @@ public class MapsActivity extends FragmentActivity
     public void isUserInPOI() {
         String TAG = "Maps Activity";
         Log.i(TAG, "Clicked ");
-        FSL.getLastLocation().addOnSuccessListener(this, (OnSuccessListener<Location>) location -> {
+        FSL.getLastLocation().addOnSuccessListener(this, location -> {
             if (location != null) {
                 locationCheck.check(this, location.getLatitude(), location.getLongitude());
             } else {
                 Log.i("location", "Location returned null");
             }
         });
+    }
+
+    private void createLocationCallback() {
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                currentLocation = locationResult.getLastLocation();
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("SingleUser");
+                myRef.setValue(currentLocation);
+            }
+        };
+    }
+
+    private void createLocationRequest() {
+        locationRequest = LocationRequest.create()
+                .setInterval(100)
+                .setFastestInterval(3000)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setMaxWaitTime(100);
+    }
+
+    @SuppressLint("MissingPermission")
+    private void startLocationUpdates() {
+        FSL.requestLocationUpdates(locationRequest,
+                locationCallback,
+                Looper.getMainLooper());
     }
 
 }
